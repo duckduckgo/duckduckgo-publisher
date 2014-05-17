40231493983258df3ed144898524567e136d1225
Doug Brown <doug@duckduckgo.com>
2014-05-17 14:41:29 -0400

    'hero' search form, add 'js-' binding classes to all static forms

--- lib/DDG/Publisher/Site/Duckduckgo/Root.pm
+++ lib/DDG/Publisher/Site/Duckduckgo/Root.pm
@@ -278,6 +278,12 @@ sub pages {{
 			text => "<span>Answers to your questions from the best sources,</span> <span>developed by our open source community.</span>",
 			type => "answers",
 			link => "/?q=what+rhymes+with+duck",
+		},{
+			title => '<b>Try DuckDuckGo</b>',
+			raw_title => 'Try DuckDuckGo',
+			hero_form => 1,
+			type => "home",
+			link => "/",
 		}],
 	},
 
