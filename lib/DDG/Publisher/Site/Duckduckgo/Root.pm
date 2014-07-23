6f8d8234405d3a74777603cc96b7a5ec4058fd4c
Doug Brown <doug@duckduckgo.com>
2014-07-23 16:13:52 -0400

    (styleguide) added color palette

--- lib/DDG/Publisher/Site/Duckduckgo/Root.pm
+++ lib/DDG/Publisher/Site/Duckduckgo/Root.pm
@@ -265,6 +265,7 @@ sub pages {{
 		alt_header_text => 1,
 		menu_hide_themes => 1,
 		icons => ['plus','clear-field','home','chev-down','chev-up','more','move','user','star','close-menu','menu','grid','check-sign','eye','loupe','check','region','cry','uploaded','prev','next','close-grid','marker','arrow-left','arrow-right','arrow-up','arrow-down',],
+		colors => ['white','white-alt','silver','ash','grey','dim','jet','red','green','teal','cyan','blue','blue-dark','blue-pale','orange','salmon','salmon-dark',],
 	},
 	supportus => sub {},
 	whatsnew => sub {
