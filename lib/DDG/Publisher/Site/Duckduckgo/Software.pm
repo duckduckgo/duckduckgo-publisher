bea62667b5a583fb0cf15e377c77a4ddb5540a77
Zaahir Moolla <moollaza@duckduckgo.com>
2018-03-21 10:35:04 -0400

    Demo, testing Publisher with subdirectory on page path

---
+++ lib/DDG/Publisher/Site/Duckduckgo/Software.pm
@@ -0,0 +1,38 @@
+package DDG::Publisher::Site::Duckduckgo::Software;
+
+use strict;
+use MooX;
+
+with qw(
+	DDG::Publisher::DirRole
+);
+
+sub path { '/software/' }
+
+sub pages {{
+	"mozilla-firefox-portable" => sub {
+		about_footer => 1,
+		no_footer_arrow => 1,
+		copyright_footer => 1,
+		copyright_text => 'Privacy, simplified.',
+		no_content_internal => 1,
+		no_spacer => 1,
+		hero_header => 1,
+		no_hero_header_icon => 1,
+		js_page_type => 'Apps',
+	},
+	
+	"notepadpp-portable" => sub {
+		about_footer => 1,
+		no_footer_arrow => 1,
+		copyright_footer => 1,
+		copyright_text => 'Privacy, simplified.',
+		no_content_internal => 1,
+		no_spacer => 1,
+		hero_header => 1,
+		no_hero_header_icon => 1,
+		js_page_type => 'Apps',
+	}
+}}
+
+1;
