9f35a8a2b56997c4abf049ba25592730509b9057
Zaahir Moolla <moollaza@duckduckgo.com>
2018-04-18 11:14:11 -0400

    Remove duplicated code, add software disclaimer footer

--- lib/DDG/Publisher/Site/Duckduckgo/Software.pm
+++ lib/DDG/Publisher/Site/Duckduckgo/Software.pm
@@ -9,52 +9,50 @@ with qw(
 
 sub path { '/software/' }
 
+my %page_defaults = (
+	about_footer => 1,
+	software_footer => 1,
+	no_footer_arrow => 1,
+	copyright_footer => 1,
+	copyright_text => 'Privacy, simplified.',
+	no_content_internal => 1,
+	no_spacer => 1,
+	js_page_type => 'Software',
+	page_class => 'product-page',
+);
+
 sub pages {{
 	"mozilla-firefox-portable" => sub {
-		about_footer => 1,
-		no_footer_arrow => 1,
-		copyright_footer => 1,
-		copyright_text => 'Privacy, simplified.',
-		no_content_internal => 1,
-		no_spacer => 1,
-		js_page_type => 'Software',
-		page_class => 'product-page',
+		%page_defaults,
+		
 		title => 'Mozilla Firefox, Portable Edition',
-
 		product => {
 			name => 'Mozilla Firefox',
 			asset_path => 'mozilla-firefox',
-			version => '59.0.1',
+			version => '59.0.2',
 			category => 'Browsers',
 			url => 'https://portableapps.com/apps/internet/firefox_portable',
 			dl => {
 				size => '74MB',
-				url => 'FirefoxPortable_59.0.1_English.paf.exe'
+				url => 'FirefoxPortable_59.0.2_English.paf.exe'
 			},
 			inst_size => '268MB',
 			summary => "Mozilla Firefox® is a fast, full-featured web browser that's easy to use. It has lots of great features including popup-blocking, tabbed-browsing, integrated search, improved privacy features, automatic updating and more. Plus, thanks to the PortableApps.com launcher bundled in the Mozilla Firefox, Portable Edition, it leaves no personal information behind on the machine you run it on, so you can take your favorite browser along with all your favorite bookmarks and extensions with you wherever you go. Firefox Portable is a dual-mode 32-bit and 64-bit app, ensuring Firefox runs as fast as possible on every PC.",
 			details => {
 				'Publisher' => 'Mozilla & PortableApps.com (John T. Haller)',
-				'Date Updated' => '2018-03-17',
+				'Date Updated' => '2018-03-28',
 				'System Requirements' => 'Windows 7, 8, 10 & WINE',
 				'License' => 'Open Source (MPL/GPL/LGPL under Mozilla EULA)',
 				'Source' => 'Launcher (included), Firefox',
-				'MD5 Hash' => '79f81c1d5ef0a2d7cfce914e552b7b6b (English)',
+				'MD5 Hash' => '968bf8179afb917293659ea3859a28ad (English)',
 			}
 		}
 	},
 
 	"q-dir-portable" => sub {
-		about_footer => 1,
-		no_footer_arrow => 1,
-		copyright_footer => 1,
-		copyright_text => 'Privacy, simplified.',
-		no_content_internal => 1,
-		no_spacer => 1,
-		js_page_type => 'Software',
-		page_class => 'product-page',
-		title => 'Q-Dir, Portable Edition',
+		%page_defaults,
 
+		title => 'Q-Dir, Portable Edition',
 		product => {
 			name => 'Q-Dir',
 			asset_path => 'q-dir',
@@ -79,16 +77,9 @@ sub pages {{
 	},
 
 	"portableapps" => sub {
-		about_footer => 1,
-		no_footer_arrow => 1,
-		copyright_footer => 1,
-		copyright_text => 'Privacy, simplified.',
-		no_content_internal => 1,
-		no_spacer => 1,
-		js_page_type => 'Software',
-		page_class => 'product-page',
-		title => 'PortableApps Platform',
+		%page_defaults,
 
+		title => 'PortableApps Platform',
 		product => {
 			is_platform => 1,
 			name => 'PortableApps',
